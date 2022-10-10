ae0869dc93a6a34763d49c464bca37b2ee193a6d
vietnguyena1 <35227782+vietnguyena1@users.noreply.github.com>
2022-10-10 07:56:26 +0700

    create react app

---
+++ demo5/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="container">
+          <ul>
+            <li style={{ '--i': 6 }}>0</li>  
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
