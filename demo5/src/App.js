ed66bd1354ce55dcbba05bff2e44696f90bd7d30
vietnguyena1 <35227782+vietnguyena1@users.noreply.github.com>
2022-10-10 08:01:28 +0700

    f2

--- demo5/src/App.js
+++ demo5/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <div class="container">
           <ul>
             <li style={{ '--i': 6 }} onMouseOver={()=>{setCount(count +1)}}>{count}</li>  
+            <li style={{ '--i': 5 }} onMouseOver={()=>{setCount(count +1)}}>{count}</li> 
           </ul>
         </div>
       </header>
