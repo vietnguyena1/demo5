2f480419e7abc5a2c311ce99b9eece012da89a11
vietnguyena1 <35227782+vietnguyena1@users.noreply.github.com>
2022-10-10 07:59:51 +0700

    f1-count

--- demo5/src/App.js
+++ demo5/src/App.js
@@ -1,14 +1,14 @@
-
+import {useState} from 'react';
 import './App.css';
 
 function App() {
-
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <div class="container">
           <ul>
-            <li style={{ '--i': 6 }}>0</li>  
+            <li style={{ '--i': 6 }} onMouseOver={()=>{setCount(count +1)}}>{count}</li>  
           </ul>
         </div>
       </header>
